4c7fc7fe10b31addfd51df0c20a252b061145e4a
fedeyak <fedeyak@gmail.com>
2025-03-18 12:18:26 +0300

    Brushed up a bit

--- code/sq-ch11-ex1/src/main/java/com/example/config/ProjectConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "com.example.proxy")
-public class ProjectConfig {
-}
